602bf0363bf9c47324ec76b7e2646d4121d035f3
Anirudha <anirudhakolekar65@gmail.com>
2026-02-09 12:07:48 +0530

    app setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    
+    <><h2 className='text-center text-red-400 font-bold text-3xl'>Welcome !!</h2>
+    </>
+  )
+}
+
+export default App
